57149769a461955245132e79c7280fdc1969d716
unknown <krzysztof0bojakowski@gmail.com>
2014-11-30 04:01:21 +0100

    Processor structure included, project overview (schema)

---
+++ chinski_sop/processor.h
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stack>
+struct processor
+{
+  uint16_t a,b,c,d; // 16bit registers of the processor, a , b , c , d
+  uint16_t cs; // code segment, hold where code begins in memory
+  uint16_t ip; // instruction pointer, it's more like instruction counter, it counts the instructions
+  short quant_time; // the number of instructions readed at one time from memory
+  std::stack <uint16_t> processor_stack; // it's a buffor for memory
+};
