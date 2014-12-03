3f45845cd17664ea253579abea641acd1cbef181
unknown <krzysztof0bojakowski@gmail.com>
2014-12-03 04:10:01 +0100

    processor architecture is almost done, added class to handle whole processor structure

--- chinski_sop/sop/processor/processor.cpp
+++ chinski_sop/sop/processor/processor.cpp
@@ -1,1 +1,132 @@
 #include "./sop/processor/processor.h"
+
+void sop::processor::ProcessorHandler::clearProcessor(sop::processor::processor *proc)
+{
+  proc->a = 0;
+  proc->b = 0;
+  proc->c = 0;
+  proc->d = 0;
+  proc->cs = 0;
+  proc->ip = 0;
+  ProcessorHandler::clearStack(proc);
+  proc->quant_time = 3;
+  proc->sign_flag = 0;
+  proc->zero_flag = 0;
+}
+
+uint16_t* sop::processor::ProcessorHandler::charRegisterHandler(sop::processor::processor *proc, char processor_register)
+{
+  if (processor_register == 'a' || processor_register == 'A')
+  {
+    return &proc->a;
+  }
+  else if (processor_register == 'b' || processor_register == 'B')
+  {
+    return &proc->b;
+  }
+  else if (processor_register == 'c' || processor_register == 'C')
+  {
+    return &proc->c;
+  }
+  else if (processor_register == 'd' || processor_register == 'D')
+  {
+    return &proc->d;
+  }
+ 
+}
+void sop::processor::ProcessorHandler::registerIncrement(sop::processor::processor *proc, char processor_register)
+{
+  uint16_t* reg = ProcessorHandler::charRegisterHandler(proc, processor_register);
+  ++(*reg);
+}
+
+void sop::processor::ProcessorHandler::registerDecrement(sop::processor::processor *proc, char processor_register)
+{
+  uint16_t* reg = ProcessorHandler::charRegisterHandler(proc, processor_register);
+  --(*reg);
+}
+
+void sop::processor::ProcessorHandler::registerIncrementByValue(sop::processor::processor *proc, char processor_register, uint16_t value)
+{
+  uint16_t* reg = ProcessorHandler::charRegisterHandler(proc, processor_register);
+  (*reg) += value;
+}
+
+void sop::processor::ProcessorHandler::registerDecrementByValue(sop::processor::processor *proc, char processor_register, uint16_t value)
+{
+  uint16_t* reg = ProcessorHandler::charRegisterHandler(proc, processor_register);
+  (*reg) -= value;
+}
+
+void sop::processor::ProcessorHandler::setRegisterField(sop::processor::processor *proc, char processor_register, uint16_t value)
+{
+  uint16_t* reg = ProcessorHandler::charRegisterHandler(proc, processor_register);
+  (*reg) = value;
+}
+
+void sop::processor::ProcessorHandler::copySourceRegisterToDestinationRegister(sop::processor::processor *proc, char source_processor_register, char destination_processor_register)
+{
+  uint16_t* src_reg = ProcessorHandler::charRegisterHandler(proc, source_processor_register);
+  uint16_t* dst_reg = ProcessorHandler::charRegisterHandler(proc, destination_processor_register);
+  (*dst_reg) = (*src_reg);
+}
+
+void sop::processor::ProcessorHandler::addSourceRegisterToDestinationRegister(sop::processor::processor *proc, char source_processor_register, char destination_processor_register)
+{
+  uint16_t* src_reg = ProcessorHandler::charRegisterHandler(proc, source_processor_register);
+  uint16_t* dst_reg = ProcessorHandler::charRegisterHandler(proc, destination_processor_register);
+  (*dst_reg) += (*src_reg);
+}
+
+void sop::processor::ProcessorHandler::subtractDestinationRegisterBySourceRegister(sop::processor::processor *proc, char source_processor_register, char destination_processor_register)
+{
+  uint16_t* src_reg = ProcessorHandler::charRegisterHandler(proc, source_processor_register);
+  uint16_t* dst_reg = ProcessorHandler::charRegisterHandler(proc, destination_processor_register);
+  (*dst_reg) -= (*src_reg);
+}
+
+void sop::processor::ProcessorHandler::incrementInstructionPointer(sop::processor::processor *proc)
+{
+  proc->ip++;
+}
+
+void sop::processor::ProcessorHandler::setInstructionPointer(sop::processor::processor *proc, short value)
+{
+  proc->ip = value;
+}
+
+void sop::processor::ProcessorHandler::loadWhereCodeSegmentStarts(/* process *p */ sop::processor::processor *proc)
+{
+  // to be done
+}
+
+void sop::processor::ProcessorHandler::setSignFlag(sop::processor::processor *proc)
+{
+  proc->sign_flag = true;
+}
+
+void sop::processor::ProcessorHandler::setZeroFlag(sop::processor::processor *proc)
+{
+  proc->zero_flag = true;
+}
+
+void sop::processor::ProcessorHandler::unsetSignFlag(sop::processor::processor *proc)
+{
+  proc->sign_flag = false;
+}
+
+void sop::processor::ProcessorHandler::unsetZeroFlag(sop::processor::processor *proc)
+{
+  proc->zero_flag = false;
+}
+
+// to be done
+//void sop::processor::ProcessorHandler::addToStack(); 
+//void sop::processor::ProcessorHandler::popFromStack 
+void sop::processor::ProcessorHandler::clearStack(sop::processor::processor *proc)
+{
+  while (!proc->processor_stack.size()==0)
+  {
+    proc->processor_stack.pop();
+  }
+}
--- chinski_sop/sop/processor/processor.h
+++ chinski_sop/sop/processor/processor.h
@@ -16,8 +16,60 @@ namespace sop
       uint16_t ip; // instruction pointer, it's more like instruction counter, it counts the instructions
       short quant_time; // the number of instructions readed at one time from memory
       std::stack <uint16_t> processor_stack; // it's a buffor for memory
+      bool zero_flag; // if (false) then operation result is not equal to 0, otherwise is equal.
+      bool sign_flag; // if (false) then operation result is positive (>=0), otherwise is negative (<0)
+    
+      processor(){ // constructor
+        a = b = c = d = 0;
+        ip = 0;
+        quant_time = 3;
+      }
     };
 
+
+    class ProcessorHandler // this class will handle any operation done on the processor fields
+    {
+
+    public:
+      //General functions 
+      static void clearProcessor(processor *proc); //sets default(starting) values
+      static uint16_t* charRegisterHandler(processor *proc, char processor_register); // convert chat into variable
+
+      //Command register
+      static void registerIncrement(processor *proc, char processor_register); // incrementing register field by one
+      static void registerDecrement(processor *proc, char processor_register); // decrementing register field by one
+
+      //Command register,value 
+      static void registerIncrementByValue(processor *proc, char processor_register, uint16_t value); // incrementing register field by value 
+      static void registerDecrementByValue(processor *proc, char processor_register, uint16_t value); // decrementing register field by value
+      static void setRegisterField(processor *proc, char processor_register, uint16_t value); // setting value into register field
+      
+      //Command register,register
+      static void copySourceRegisterToDestinationRegister(processor *proc, char source_processor_register, char destination_processor_register); // source means processor to copy from,
+                                                                                                                                  // and destination means processor to be pasted in
+      static void addSourceRegisterToDestinationRegister(processor *proc, char source_processor_register, char destination_processor_register); // e.g destA = destA + srcB 
+      static void subtractDestinationRegisterBySourceRegister(processor *proc, char source_processor_register, char destination_processor_register); // e.g destA = destA - srcB
+
+      //Instruction pointer functions
+      static void incrementInstructionPointer(processor *proc); // instruction pointer ++
+      static void setInstructionPointer(processor *proc, short value); // setting instruction pointer
+
+      //Code segment
+      static void loadWhereCodeSegmentStarts(/* process *p */ processor *proc); // loads where code begins in memory
+
+      //Flags
+      static void setZeroFlag(processor *proc); // setting zero flag to true
+      static void unsetZeroFlag(processor *proc); // setting zero flag to false
+      static void setSignFlag(processor *proc); // setting sign flag to true
+      static void unsetSignFlag(processor *proc); // setting sign flag to false
+
+      //Stack
+      static void addToStack(/**/);
+      static void popFromStack(/**/);
+      static void clearStack(processor *proc); // cleares all data in the stack
+
+
+    };
   }
 
 }
